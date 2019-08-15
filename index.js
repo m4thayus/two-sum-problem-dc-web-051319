1610134a4e35ef8809f127310f0906a548c69e11
Matt Williams <matt.seongjun@gmail.com>
2019-08-15 10:01:20 -0400

    better

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function cleanKeys(obj) {
 
 function twoSum(sum, array) {
     rtn = {}
-    for (let i of array) {
+    for (let i = 0; i < array.length; i++) {
         for(let j of array) {
             if (i + j === sum) {
                 rtn[Math.abs(i - j)] = ([i, j])
@@ -18,7 +18,19 @@ function twoSum(sum, array) {
     return cleanKeys(rtn)
 }
 
+function betterTwoSum(sum, array) {
+    rtn = []
+    for (let i = 0; i < array.length / 2; i++) {
+        for(let j of array) {
+            if (i + j === sum) {
+                rtn.push([i, j])
+            }
+        }
+    }
+    return rtn
+}
 let sum = 6
 let a = [5, -2, 4, 9, 1]
 
 console.log(twoSum(sum, a))
+console.log(betterTwoSum(sum, a))
